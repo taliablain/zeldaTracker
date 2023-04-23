9e6e9337d2e3926cc36d61d3b6749c82c62d2c48
razrobill <tb1126@york.ac.uk>
2023-04-23 18:32:07 +0100

    checkboxes

--- script.js
+++ script.js
@@ -1,17 +1,72 @@
-let boxes = document.getElementsByClassName('box').length;
+var PersistentCheckboxes = function(){
+  var genKey, load, save, addListeners, init;
 
-function save() {	
-  for(let i = 1; i <= boxes; i++){
-	  var checkbox = document.getElementById(String(i));
-    localStorage.setItem("checkbox" + String(i), checkbox.checked);	
-  }
-}
+  var PAGE = window.location.href;
 
-//for loading
-for(let i = 1; i <= boxes; i++){
-  if(localStorage.length > 0){
-    var checked = JSON.parse(localStorage.getItem("checkbox" + String(i)));
-    document.getElementById(String(i)).checked = checked;
+  /**
+   * Generates a key to store information for a checkbox in localStorage. This
+   * is in the format: checkbox:<page>:<id>, where <page> is the URL of the
+   * page and <id> is the checkbox id (passed in as a parameter).
+   * 
+   * @param {string} id The id of the checkbox.
+   * @return A key that should be unique for the given checkbox on the current
+   *         page.
+   */
+  genKey = function(id){
+      return 'checkbox:'+ PAGE +':'+ id;
   }
-}
-window.addEventListener('change', save);
+
+  /**
+   * A listener to be placed on a checkbox. Saves the checked property (true 
+   * or false) of the checkbox provided in localStorage using its id as the 
+   * key. Checkboxes without an id are not saved.
+   *
+   * @param event The triggered event (ignored). 
+   */
+  save = function(event){
+      // Only inputs with an id will be saved.
+      if(this.id === ""){ return; }
+      localStorage.setItem(genKey(this.id), this.checked+"");
+  };
+
+  /**
+   * Loads any saved checkbox statuses from localStorage.
+   */
+  load = function(){
+      jQuery('input[type="checkbox"]').each(function(i, elm){
+          // Ignore checkboxes with no id.
+          var key = genKey(elm.id);
+          if(elm.id !== "" && key in localStorage){
+              elm.checked = localStorage.getItem(key) === "true";
+          }
+      });
+  };
+
+  /**
+   * Places a listener on checkbox changes, which triggers the save function.
+   */
+  addListeners = function(){
+      jQuery(document).on('change', 'input[type=checkbox]', save);
+  };
+
+  /**
+   * Loads any saved data and adds listeners for changes to checkboxes.
+   */
+  init = function(options){
+      // Only supported if localStorage is available.
+      if(typeof(Storage) === "undefined"){ return; }
+      load();
+      addListeners();
+  };
+
+  init();
+
+  return this;
+};
+
+
+// Let's get this party started!
+var persistentCheckboxes;
+jQuery(document).ready(function(){
+  persistentCheckboxes = new PersistentCheckboxes();
+});
